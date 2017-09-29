156933b6ac029b2bd3077b44351e93a45752d9d3
Jonathan Greene <jonathangreene@Jonathans-MacBook-Pro.local>
2017-09-29 19:34:58 -0400

    Adding latest version of code"

--- guess-the-word/SQL Generator.py
+++ guess-the-word/SQL Generator.py
@@ -3,35 +3,57 @@
 # length = len(word)
 # right = list("_" * length)
 # finished = False
+#import sy
+#stillAlive = False
 
-stillAlive = False
+#Declare values
+tables=[]
+joins=[]
+joinType=["INNER JOIN","RIGHT JOIN","LEFT JOIN","FULL OUTER JOIN"]
+stillAlive = True
+
+#--------------------------------------------
+#  Prompt for User database and table Input
+#--------------------------------------------
+
 DBInput = input("Database being accessed:")
-tableInput = input("Insert table to select from:")
+tableInput = input('Insert table to select from:')
 DBTable = DBInput + ".dbo" + "." + tableInput
-stillAlive = "Y"
-while stillAlive == "Y":
-    stillAlive = input("Do you need to join to another table Y/N:")
-    joinType = input("How would you like to join to the table.(I, R, L, F)?:")
-    if joinType == "I":
-                joinType = "INNER JOIN"
-                tableInput2 = input("Insert table to join to:")
-                DBTable2 = DBInput + ".dbo" + "." + tableInput2
-    stillAlive = input("Do you need to join to another table Y/N:")
-else:
-    stillAlive = "N"
-print("SELECT * FROM " + DBTable + " " + joinType + " " + DBTable2 + " ON " + "????? = ?????")
+tables.append(DBTable)
+
+#--------------------------------------------
+#  Multiple Joins Step/Loop
+#--------------------------------------------
+
+while stillAlive == True:
+    keepGoing = input("Do you need to join to another table Y/N:")
+    if keepGoing not in ("Y", "N"):
+        print("Invalid value entered: " + str(keepGoing))
+        stillAlive = False
+    else:
+        joinInput = input("How would you like to join to the next table.(I, R, L, F)?:")
+        for character in joinInput:
+            if character not in ('I','R','L','F'):
+                print("Invalid value entered: " + str(character))
+        joins.append(joinInput)
+        DBInput = input("Database being accessed:")
+        tableInput = input('Insert table to select from:')
+        DBTable = DBInput + ".dbo" + "." + tableInput
+        tables.append(DBTable)
+
+print("""
+SELECT * 
+FROM """ + tables[0] + " " + joinType[0] + " " + tables[1] + " ON " + "????? = ?????")
+
+#while stillAlive == "Y":
+#    joinType = input("How would you like to join to the table.(I, R, L, F)?:")
+#    if joinType == "I":
+#        joinType = list("INNER JOIN")
+#    elif joinType == "R":
+#        joinType = list("RIGHT JOIN")
+#    elif joinType == "L":
+#        joinType = list("LEFT JOIN")
+#    else:
+#        joinType == list("FULL OUTER JOIN")
 
 
-# while finished == False:
-#    guess = input("Guess a letter!")
-#    if guess not in master:
-#        print("This letter is not in the word.")
-#    for letter in word:
-#            if letter == guess:
-#                index = word.index(guess)
-#                right[index] = guess
-#                word[index] = "_"
-#                print(right)
-#            if list(master) == right:
-#                print("You win!")
-#                finished = True
---
+++ guess-the-word/Test.py
@@ -0,0 +1,141 @@
+#boring = False
+#beloved = True
+#if not boring or beloved:
+#    print("watch movie ")
+#if not boring and beloved:
+#    print("but ask them first")
+
+#a = True
+#b = True
+#if a and b:
+#    print("a and b are true")
+#if a or b:
+#    print("a, b, or both are true")
+
+#score = 200
+#try:
+#    score
+#    print(str(score))
+#except:
+#    print("score wasn't set")
+#    score = 100
+
+
+
+#try:
+#    print(number)
+#except:
+#    print("number wasn't set")
+
+
+#use If statements for and booleans
+#love = False
+#score = 90
+#if love == True:
+#    print("Aww!")
+#elif love == False and score < 100:
+#    print("Go for it")
+#else:
+#    print("Won!")
+
+#start from back of a list
+#l = ["a","b","c","d","j","m"]
+#print(l[-1])
+
+#Add to a list
+#romance = ["Juno", "Her", "Chocolat"]
+#romance.append("Alien")
+#print(len(romance))
+
+#add mutliple lists to a single list and access by indexs.
+#l1 = ["a","b", "c", "d"]
+#l2 = ["z","g","m"]
+#l3 = [22,3,6,0.1,2,"Hello",1]
+#listlist = [l1,l2,l3]
+#print(listlist[2][1])
+
+
+#using tuples for read only values becuase values can not be changed
+#tup1 = ("America","conq. 1492")
+#tup2 = ("light","222 km/s")
+#print(tup2[1])
+
+#index using tuple
+#myTuple = ("north","south")
+#print(myTuple[0:1])
+
+#turn a tumple into a list and change a value
+#mytuple = ("Ann", "Ben","Chen")
+#mylist = list(mytuple)
+#mylist[1]= "Bob"
+#print(mylist)
+
+
+#list stored multiple values at once and accessed and changed
+#shoppinglist = ["ham","eggs","flour"]
+#shoppinglist.append([3,"a",True])
+#print(shoppinglist)
+
+
+# for loop will loop 4 times
+#for i in range(4):
+#    print(i)
+
+#for loop with list
+#mylist= ["me", "you", "we","us"]
+#for element in mylist:
+#    print(element)
+
+#for loop with index counter
+#mylist = [13,21,4,7]
+#changes = [5,4,3,2]
+#for i in range(len(mylist)):
+#    mylist[i] += changes[i]
+#    print(str(mylist[i]))
+
+#tell loop to stop
+#for i in range(8):
+#    print(i)
+#    if i == 4:
+#        break
+
+
+#while loop
+#temperature = 60
+#summer = True
+#while temperature >= 60:
+#    temperature -= 1
+#summer = False
+#print(temperature)
+
+#infinite while loop never true
+#d = 0
+#while d >= 0:
+#    d += 1
+#    print(d)
+
+#interate over a string
+#text = "One ring to rule them all"
+#found = False
+#for character in text:
+#    if character == "z":
+#        found = True
+#print("Found: " + str(found))
+
+#loop within a loop
+#students = [
+#    ["John", "CS"],
+#    ["Lisa", "Physics"]]
+#for i in range(len(students)):
+#    for j in range(len(students[i])):
+#        print(students[i][j])
+
+#loop until conditions are true
+#for i in range(3):
+#    print("for loop #" + str(i))
+#bee = "bee"
+#while bee != "beeeee":
+#    bee += "e"
+#    print(bee)
+
+
